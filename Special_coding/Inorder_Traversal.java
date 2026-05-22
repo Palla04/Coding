b82543c696e7c2b151c66f352b4be473327df63f
Pallabi Biswas <pallabibiswas4002@gmail.com>
2026-05-22 20:44:37 +0530

    One more is added

---
+++ Special_coding/Inorder_Traversal.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+// Definition for a binary tree node.
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {}
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Inorder_Traversal {
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        inorder(root, result);
+        return result;
+    }
+
+    private void inorder(TreeNode node, List<Integer> result) {
+        if (node == null) {
+            return;
+        }
+
+        inorder(node.left, result);
+        result.add(node.val);
+        inorder(node.right, result);
+    }
+
+    // Main method for VS Code
+    public static void main(String[] args) {
+
+        /*
+                1
+                 \
+                  2
+                 /
+                3
+        */
+
+        TreeNode root = new TreeNode(1);
+        root.right = new TreeNode(2);
+        root.right.left = new TreeNode(3);
+
+        Inorder_Traversal sol = new Inorder_Traversal();
+
+        List<Integer> ans = sol.inorderTraversal(root);
+
+        System.out.println(ans);
+    }
+}

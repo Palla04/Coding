d188152f69a8a00b1ccdd59a138fcd5c1abf30d0
Pallabi Biswas <pallabibiswas4002@gmail.com>
2026-09-24 19:58:09 +0530

    8 patterns are added

---
+++ Patterns/pattern_8.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void pattern8(int n) {
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<i;j++){
+                System.out.print(" ");
+            }
+
+            for(int j=1;j<=2*(n-i)+1;j++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
